99b102776c0e062276d8f9acf231e09761448d2b
Mo <cs012002@coderacademy.edu.au>
2020-07-11 01:13:34 +0000

    finished charactersearch function

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 mapf = open("map.txt", "r")
-line = []
+rows = []
 for x in mapf:
-    line.append(x)
+    rows.append(x)
 
 def searchcharacter():
-    
+    for x in range(0,len(rows)):
+        try:
+            return x, rows[x].index("*")
+        except:
+            continue
 
 def fromcoord(row, column):
     hslice1 = column - 10
-    hslice2 = column + 10
-    vslice1 = row - 2
-    vslice2 = row + 3
+    hslice2 = column + 11
+    vslice1 = row - 3
+    vslice2 = row + 4
     for x in range(vslice1, vslice2):
-        print(line[x][hslice1:hslice2])
+        print(rows[x][hslice1:hslice2])
 
-fromcoord(4, 24)
+fromcoord(searchcharacter()[0], searchcharacter()[1])
 
     
