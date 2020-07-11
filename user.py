6ba08998712e3942f7a9078f9c2fa3c726547318
Mo <cs012002@coderacademy.edu.au>
2020-07-11 09:47:48 +0000

    added 4 directional movement and refined dead zones

--- user.py
+++ user.py
@@ -2,19 +2,31 @@ import fetch
 
 def charactermove(direction="right"):
     rows = fetch.getmap()
-    x = fetch.searchcharacter(fetch.getmap())[0]
-    y = fetch.searchcharacter(fetch.getmap())[1]
+    y = fetch.searchcharacter(fetch.getmap())[0]
+    x = fetch.searchcharacter(fetch.getmap())[1]
     newrow = ""
     if direction == "right":
-        if rows[x][y + 1] == ".":
-            rows[x] = rows[x].replace("*", ".")
-            newrow += rows[x][:y+1] + "*" + rows[x][y+2:]
-            rows[x] = newrow
+        if rows[y][x + 1] == ".":
+            rows[y] = rows[y].replace("*", ".")
+            newrow += rows[y][:x+1] + "*" + rows[y][x+2:]
+            rows[y] = newrow
             fetch.writetomap(rows)
     elif direction == "left":
-        if rows[x][y - 1] == ".":
-            rows[x] = rows[x].replace("*", ".")
-            newrow += rows[x][:y-1] + "*" + rows[x][y:]
-            rows[x] = newrow
+        if rows[y][x - 1] == ".":
+            rows[y] = rows[y].replace("*", ".")
+            newrow += rows[y][:x-1] + "*" + rows[y][x:]
+            rows[y] = newrow
+            fetch.writetomap(rows)
+    elif direction == "down":
+        if rows[y + 1][x] == ".":
+            rows[y] = rows[y].replace("*", ".")
+            newrow += rows[y+1][:x] + "*" + rows[y+1][x+1:]
+            rows[y+1] = newrow
+            fetch.writetomap(rows)
+    elif direction == "up":
+        if rows[y - 1][x] == ".":
+            rows[y] = rows[y].replace("*", ".")
+            newrow += rows[y-1][:x] + "*" + rows[y-1][x+1:]
+            rows[y-1] = newrow
             fetch.writetomap(rows)
     fetch.fromcoord(fetch.getmap(), fetch.searchcharacter(fetch.getmap())[0], fetch.searchcharacter(fetch.getmap())[1])
